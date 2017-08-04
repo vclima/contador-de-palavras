25b0f37aeac164b71a25bdb85b6dbc58b4342bc2
vclima <victor.lima.314@gmail.com>
2017-08-04 17:30:22 -0300

    Finalizado

--- src/main.c
+++ src/main.c
@@ -6,24 +6,47 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main() {
 
-  char c;
+  char c,*teste,ant;
+  char separadores[9];
   int palavras;
-  c = 1;
+  short estado;
+  strcpy(separadores," .,:;-!?");
   palavras=0;
-  scanf("%c", &c);
-  if(c!='\n'){
-    palavras++;
-    while (c != '\n') {
-      scanf("%c", &c);
-      if(c==' '){
-        palavras++;
+  ant='\0';
+  estado=0;
+  scanf("%c",&c);
+  teste=strchr(separadores,c);
+  if(teste==NULL){
+    palavras++; //A string não é uma string de caracteres de pontuação
+  }
+  while(c!='\n'){
+    estado=0;
+    ant=c;
+    scanf("%c",&c);
+    teste=strchr(separadores,c);
+    if(teste!=NULL){
+      estado=1;
+      palavras++;
+    }
+    while(estado==1){
+      scanf("%c",&c);
+      teste=strchr(separadores,c);
+      if(c=='\n'){
+        palavras--;
+      }
+      if(teste==NULL&&isdigit(c)&&isdigit(ant)){
+        palavras--;
+      }
+      if(teste==NULL){
+        estado=0;
       }
     }
   }
-
   printf("%d\n",palavras);
   return 0;
 }
